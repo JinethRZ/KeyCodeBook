1b58603b8f98e5a08b952a027914b4382acb9565
JinethRZ <jineth.rodriguezz@gmail.com>
2020-10-28 14:25:25 -0500

    Creación de los métodos de obtener y eliminar a libros y usuarios.

--- controllers/book.js
+++ controllers/book.js
@@ -64,3 +64,56 @@ exports.update = (req, res) => {
         }
     )
 }
+
+/**Línea exports.getAll en adelante
+ * 
+ * Método para listar todos los libros que están en la plataforma.
+ * @param {*} req => Todo lo que se recibe.
+ * @param {*} res => Respuesta que se devuelve.
+ */
+
+exports.getAll = (req, res) => {
+    BookModel.find()
+        .populate('genre') //populate: método que permite traer los datos de la colección con la que se tiene la relación. Recibe una de las opciones del campo que tiene la relación. 
+        .exec() //exec: ejecuta la consulta para poder continuar, va de la mano con .populate. Este junto a .populate, sólo se usa cuando hay una relación.
+        .then((books) => { res.send(books)})//se coloca en plural el nombre de la colección.
+        .catch((error) => {
+            res.status(500).send({message: error.message})
+        })
+}
+
+/**Línea exports.getOne en adelante
+ * 
+ * Método para listar sólo un libro que está en la plataforma. Se obtiene a través del id.
+ * @param {*} req => Todo lo que se recibe.
+ * @param {*} res => Respuesta que se devuelve.
+ */
+
+exports.getOne = (req, res) => {
+    BookModel.findById(req.params.id)        
+    .populate('genre') 
+    .exec() 
+    .then((book) => { res.send(book)})
+    .catch((error) => {
+        res.status(500).send({message: error.message})
+    })
+}
+
+/**Línea exports.deleteOne en adelante
+ * 
+ * Método para eliminar un libro por el id.
+ * @param {*} req => Todo lo que se recibe.
+ * @param {*} res => Respuesta que se devuelve.
+ */
+
+exports.deleteOne = (req, res) => {
+    BookModel.findByIdAndRemove(req.params.id)
+        .then((book) => { res.send(book) })
+        .catch(
+            (error) => {
+                res.status(500).send({
+                    message: error.message
+                })
+            }
+        )
+}
