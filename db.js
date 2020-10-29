b6f7e090a6dac0d649824b0453b674431b51fbf8
JinethRZ <jineth.rodriguezz@gmail.com>
2020-10-29 17:34:58 -0500

    Variable de conexión de mongoDB agregada en db

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose') //Moongose: paquete que permite la comunicación con nuestra base de datos. 
+const config = require ('./config') //
 
 const conectDB = () => {
-    mongoose.connect('', {useNewUrlParser: true, useUnifiedTopology: true}, (error) => { //useNewUrlParser: analiza la información que se le quiere enviar a Mongo. useUnifiedTopology: escucha los llamados que se le hacen a mongoDB y monitorea qué es lo que pasa. 
+    mongoose.connect(config.mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, (error) => { //useNewUrlParser: analiza la información que se le quiere enviar a Mongo. useUnifiedTopology: escucha los llamados que se le hacen a mongoDB y monitorea qué es lo que pasa. 
         if(error){
             console.log('Error: ', error)
         }else{
